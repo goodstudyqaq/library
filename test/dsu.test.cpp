cb6fa0ee110c38261a88a6122bcb4a15557b740e
goodstudy <goodstudyQAQ@163.com>
2022-10-06 10:48:54 +0800

    add some example

---
+++ test/dsu.test.cpp
@@ -0,0 +1,46 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "src/data_structure/dsu.hpp"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#else
+#define debug(...) 42
+#endif
+
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+struct fast_ios {
+    fast_ios() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    };
+} fast_ios_;
+
+int main() {
+#ifdef LOCAL
+    freopen("./data.in", "r", stdin);
+#endif
+    int n, q;
+    cin >> n >> q;
+
+    DSU2 dsu(n);
+
+    for (int i = 0; i < q; i++) {
+        int t, u, v;
+        cin >> t >> u >> v;
+        if (t == 0) {
+            dsu.merge(u, v);
+        } else {
+            bool f = (dsu.leader(u) == dsu.leader(v));
+            cout << f << endl;
+        }
+    }
+    return 0;
+}
