250e3cebd3d0140a44af043f4cb7d28546aaf2cb
shen <goodstudyQAQ@163.com>
2024-05-26 22:50:43 +0200

    update primer

--- math/prime.hpp
+++ math/prime.hpp
@@ -10,28 +10,47 @@ struct Prime {
     vector<int> min_factor;
     vector<int> euler_phi;
     vector<int> mobius;
+    vector<int> prm_num;
+
+    vector<int> max_prm;
     // O(n)
-    Prime(int n) {
+    int n;
+    Prime(int n) : n(n) {
         min_factor.resize(n + 1);
         euler_phi.resize(n + 1);
         mobius.resize(n + 1);
+        prm_num.resize(n + 1);
         for (int i = 2; i <= n; i++) {
             if (min_factor[i] == 0) {
                 min_factor[i] = i;
                 primes.push_back(i);
                 euler_phi[i] = i - 1;
                 mobius[i] = -1;
+                prm_num[i] = 1;
             }
             for (int j = 0; j < primes.size() && primes[j] <= min_factor[i] && i * primes[j] <= n; j++) {
                 min_factor[i * primes[j]] = primes[j];
                 if (min_factor[i] == primes[j]) {
                     euler_phi[i * primes[j]] = euler_phi[i] * primes[j];
                     mobius[i * primes[j]] = 0;
+                    prm_num[i * primes[j]] = prm_num[i];
                 } else {
                     euler_phi[i * primes[j]] = euler_phi[i] * (primes[j] - 1);
                     mobius[i * primes[j]] = -mobius[i];
+                    prm_num[i * primes[j]] = prm_num[i] + 1;
                 }
             }
+        }
+    }
+
+    void init_max_prm() {
+        // 预处理 [2, n] 每个数的最大质因数
+        for (int i = 2; i <= n; i++) {
+            if (min_factor[i] == i) {
+                // 质数
+                max_prm[i] = i;
+            }
+            max_prm[i] = max_prm[i / min_factor[i]];
         }
     }
 };
